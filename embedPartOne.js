9b8fd4213e044249b2969bf8823fe6eb85095825
ironapps <55113166+ironapps@users.noreply.github.com>
2019-11-06 16:14:56 -0700

    Create embedPartOne.js

---
+++ embedPartOne.js
@@ -0,0 +1,6 @@
+document.write( '<script src=\"https://ironapps.github.io/Video/IronPlayer.js\">\n' );
+document.write( '</script>\n' );
+document.write( '<html><head><script src=\"https://ironapps.github.io/Video/Skin.js\"></script>\n' );
+document.write( '</head><body><main id=\"canvas-wrap\"> <canvas width=\"800\" height=\"600\"> </canvas><link rel=\"stylesheet\" type=\"text/css\" href=\"https://ironapps.github.io/Video/Skin.css\"><div id=\"\" \'video\'=\"\">\n' );
+document.write( '<main id=\"Gui\">\n' );
+document.write( '<div id=\"player\"><video id=\"video-element\" oncontextmenu=\"return false;\" style=\"-moz-user-select: none; -webkit-user-select: none; -ms-user-select:none; user-select:none;-o-user-select:none;\" unselectable=\"on\" onselectstart=\"return false;\" onmousedown=\"return false;\">' );
